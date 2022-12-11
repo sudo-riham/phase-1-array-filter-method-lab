a7beacc8386e31bdcaf55b080ca661b2604ece72
rihamzeidan <r.zeidan@eweev.com>
2022-12-11 22:01:47 +0200

    Solution

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+function matchName(array,name) {
+    console.log(name)
+    console.log(array)
+let results = array.filter(object => object.name === name);
+console.log(results)
+return results
+}
